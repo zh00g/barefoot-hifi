600e432751e5f3295f2f59acfc1b1de4f68d34a1
zh00g <56774485+zh00g@users.noreply.github.com>
2021-03-01 21:45:23 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,185 @@
+import * as React from 'react';
+import { Button, Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+
+function TrailPreviewScreen(props) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Trail Preview!</Text>
+      <Button
+        title="Let's Go"
+        onPress={() => props.navigation.navigate('MapStart')}
+      />
+    </View>
+  );
+}
+
+function MapStartScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Map Start!</Text>
+    </View>
+  );
+}
+
+
+function CameraScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Camera screen</Text>
+    </View>
+  );
+}
+
+function ExploreScreen({ navigation }) {
+  return (
+    <View style = {styles.container} >
+    <View style = {styles.mapbox}>
+    <Text>map picture</Text>
+    </View>
+    <View style={styles.trailpics}>
+      <Text>Explore screen</Text>
+      <Button
+        title="Go to Trail Preview"
+        onPress={() => navigation.navigate('Preview')}
+      />
+    </View>
+    </View>
+  );
+}
+function FeedScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function CreateScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Create screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const ExploreStack = createStackNavigator();
+
+function ExploreStackScreen() {
+  return (
+    <ExploreStack.Navigator>
+      <ExploreStack.Screen name="Explore" component={ExploreScreen} />
+      <ExploreStack.Screen name="Preview" component={TrailPreviewScreen} />
+    </ExploreStack.Navigator>
+  );
+}
+
+const TrailPreviewStack = createStackNavigator();
+
+function TrailPreviewStackScreen() {
+  return (                                  
+    <TrailPreviewStack.Navigator>
+      <TrailPreviewStack.Screen name="Preview" component={TrailPreviewScreen} />
+      <TrailPreviewStack.Screen name="MapStart" component={MapStartScreen} />
+    </TrailPreviewStack.Navigator>
+  );                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+}
+
+
+const FeedStack = createStackNavigator();
+
+function FeedStackScreen() {
+  return (
+    <FeedStack.Navigator>
+      <FeedStack.Screen name="Feed" component={FeedScreen} />
+      <FeedStack.Screen name="Details" component={DetailsScreen} />
+    </FeedStack.Navigator>
+  );
+}
+
+const ProfileStack = createStackNavigator();
+
+function ProfileStackScreen() {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen name="Profile" component={ProfileScreen} />
+      <ProfileStack.Screen name="Details" component={DetailsScreen} />
+    </ProfileStack.Navigator>
+  );
+}
+
+const CreateStack = createStackNavigator();
+
+function CreateStackScreen() {
+  return (
+    <CreateStack.Navigator>
+      <CreateStack.Screen name="Create" component={CreateScreen} />
+      <CreateStack.Screen name="Details" component={DetailsScreen} />
+    </CreateStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Cam" component={CameraScreen} />
+        <Tab.Screen name="Feed" component={FeedStackScreen} />
+        <Tab.Screen name="Explore" component={ExploreStackScreen} />
+        <Tab.Screen name="Create" component={CreateStackScreen} />
+        <Tab.Screen name="Profile" component={ProfileStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    //alignItems: "center",
+  },
+  mapbox: {
+    flex: 1,
+    backgroundColor: "lightgreen",
+    //alignItems: "center",
+  },
+  trailpics: {
+    flex: 2,
+    backgroundColor: "lightblue",
+    //alignItems: "center",
+  },
+});
