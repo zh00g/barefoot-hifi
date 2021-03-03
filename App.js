0d786bf267234c94b7ddb3f9161b68b6905561ff
xieanthony10 <xieanthony10@gmail.com>
2021-03-03 14:58:28 -0800

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Button, Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//testing commit
 
 function DetailsScreen() {
   return (
